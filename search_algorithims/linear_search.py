919ba5544c8e829f098ce35c398623ba88cc4155
ajiksaine <ajiksaine@gmail.com>
2023-10-09 20:32:39 +0000

    Linear Search algorithim updated to take inputs

--- search_algorithims/linear_search.py
+++ search_algorithims/linear_search.py
@@ -4,7 +4,6 @@ def search(my_list,target):
     """
     Linear Search Algorithim
 
-    Check if list is empty then retun None
     Check if the value at the first position in the list and compare it with the target.
     If the value at the first position is equal to the target, return the index.
     Else check the next item on the list until you find the value or reach the end of the list.
@@ -17,8 +16,8 @@ def search(my_list,target):
     if you find the value return the position of the value
     else if it is the end of the list and it is not the value, return None
 
-    This have a time complexcity of O(n)
-    Space complexcity of O(1)
+    This have a time complexity of O(n)
+    Space complexity of O(1)
     """
 
     for n in range(len(my_list)):
@@ -35,6 +34,20 @@ def verify(result):
     else:
         print("Target not in the given list")
 
-input_list = [1,2,3,4,5,6,7,8,9,10]
 
-verify(search(input_list,7))
+# Get user input for the sorted array
+input_str = input("Enter a array of numbers, separated by spaces: ")
+input_list = [int(x) for x in input_str.split()]
+
+
+
+while True:
+    # Get user input for the target element
+    target = input("Enter the target element to search for (or exit to quit): ")
+
+    if str(target) == "exit":
+        break
+
+    key = int(target)
+
+    verify(search(input_list,key))
