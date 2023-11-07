9f5c05b08c33322115549ff6abf0b67d0189e5b2
ajiksaine <ajiksaine@gmail.com>
2023-11-07 07:06:33 +0000

    define a push method for the stack

--- stack/stack.py
+++ stack/stack.py
@@ -3,7 +3,20 @@ class Node:
         self.data = data
         self.next = None
 
+    def __repr__(self) -> str:
+        return "<Node data: %s>" % self.data
+
 class Stack:
     def __init__(self):
         self.top = None
         self.size = 0
+
+    def push(self, data):
+        # Create a node with the data
+        new_node = Node(data)
+        if self.top:
+            new_node.next = self.top
+        # Set the created node to the top node
+        self.top = new_node
+        # Increase the size of the stack by one
+        self.size += 1
