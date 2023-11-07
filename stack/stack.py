ea744f296d9814280fe6b24185898096395a7a58
ajiksaine <ajiksaine@gmail.com>
2023-11-07 06:46:22 +0000

    initialize the node and the stack class

---
+++ stack/stack.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.size = 0
